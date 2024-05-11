a1005678c96d4d5baf865bf75bfd4eedafde195a
PPandey941 <prachipandey942001100170@gmail.com>
2024-05-11 16:42:01 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express();
+
+app.get("/", (req, res) =>{
+    //root route http://localhost:5000/
+
+    res.send("Hello World");
+
+});
+
+app.listen(5000, () => console.log('server is running on this port 5000'));
