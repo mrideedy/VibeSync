a7703a77713a5b049088a1a70faa17938798ceb5
mrideedy <bca40584.20@bitmesra.ac.in>
2024-05-12 12:54:06 +0530

    auth_controller

--- backend/pumcha.js
+++
@@ -1,3 +0,0 @@
-// My baby
-//I love you my boo boo
-// I love you too pumcha
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
-const express = require("express")
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "../backend/routes/auth.routes.js"
 
 const app = express();
 
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+
 app.get("/", (req, res) =>{
     //root route http://localhost:5000/
-
     res.send("Hello World");
-
 });
 
-app.listen(5000, () => console.log('server is running on this port 5000'));
+app.use("/api/auth", authRoutes);
+app.listen(PORT, () => console.log(`server is running on this port ${PORT}`));
