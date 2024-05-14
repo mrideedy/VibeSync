388908e38eb5a8b9c8c064267c4c788c9461efe9
mrideedy <bca40584.20@bitmesra.ac.in>
2024-05-15 00:33:43 +0530

    message and conversation added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "../backend/routes/auth.routes.js"
+import messageRoutes from "../backend/routes/message.routes.js"
 import connectToMongoDB from "./db/connectToMongo.js";
 
 const app = express();
@@ -11,6 +12,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
  // To parse the incoming requests with JSON payloads (from req.body)
 
