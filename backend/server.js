f14e5ec1a9d5bb1c0a272efcec02b6945e67a766
PPandey941 <prachipandey942001100170@gmail.com>
2024-05-15 14:47:35 +0530

    backend done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 import authRoutes from "../backend/routes/auth.routes.js"
 import messageRoutes from "../backend/routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongo.js";
 
 const app = express();
@@ -11,8 +12,12 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
  // To parse the incoming requests with JSON payloads (from req.body)
 
@@ -25,3 +30,5 @@ app.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server Running on port ${PORT}`);
 });
+
+
